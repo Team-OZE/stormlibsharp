d11a6a48904f2b5b91562a883810c0605f1dce8a
Marco de Abreu <marcoabreu@users.noreply.github.com>
2020-12-30 20:42:35 +0100

    Add AddFileFromDiskEx, RemoveFile and RenameFile

--- src/StormLibSharp/MpqArchive.cs
+++ src/StormLibSharp/MpqArchive.cs
@@ -159,6 +159,30 @@ namespace StormLibSharp
                 throw new Win32Exception();
         }
 
+        public void AddFileFromDiskEx(string filePath, string archiveName, MpqFileAddFileExFlags flags, MpqFileAddFileExCompression compression, MpqFileAddFileExCompression compressionNext)
+        {
+            VerifyHandle();
+
+            if (!NativeMethods.SFileAddFileEx(_handle, filePath, archiveName, (uint)flags, (uint)compression, (uint)compressionNext))
+                throw new Win32Exception();
+        }
+
+        public void RemoveFile(string fileName)
+        {
+            VerifyHandle();
+
+            if (!NativeMethods.SFileRemoveFile(_handle, fileName, 0))
+                throw new Win32Exception();
+        }
+
+        public void RenameFile(string oldFileName, string newFileName)
+        {
+            VerifyHandle();
+
+            if (!NativeMethods.SFileRenameFile(_handle, oldFileName, newFileName))
+                throw new Win32Exception();
+        }
+
         public void Compact(string listfile)
         {
             VerifyHandle();
@@ -334,6 +358,110 @@ namespace StormLibSharp
     {
         None = 0x0,
     }
+
+    [Flags]
+    public enum MpqFileAddFileExFlags : uint
+    {
+        NONE = 0x0,
+
+        /// <summary>
+        /// The file will be compressed using IMPLODE compression method. This flag cannot be used together with MPQ_FILE_COMPRESS. 
+        /// If this flag is present, then the dwCompression and dwCompressionNext parameters are ignored. 
+        /// 
+        /// This flag is obsolete and was used only in Diablo I.
+        /// </summary>
+        MPQ_FILE_IMPLODE = 0x00000100,
+
+        /// <summary>
+        /// The file will be compressed. This flag cannot be used together with MPQ_FILE_IMPLODE.
+        /// </summary>
+        MPQ_FILE_COMPRESS = 0x00000200,
+
+        /// <summary>
+        /// The file will be stored as encrypted.
+        /// </summary>
+        MPQ_FILE_ENCRYPTED = 0x00010000,
+
+        /// <summary>
+        /// The file's encryption key will be adjusted according to file size in the archive. This flag must be used together with MPQ_FILE_ENCRYPTED.
+        /// </summary>
+        MPQ_FILE_FIX_KEY = 0x00020000,
+
+        /// <summary>
+        /// The file will have the deletion marker.
+        /// </summary>
+        MPQ_FILE_DELETE_MARKER = 0x02000000,
+
+        /// <summary>
+        /// The file will have CRC for each file sector. Ignored if the file is not compressed or if the file is stored as single unit.
+        /// </summary>
+        MPQ_FILE_SECTOR_CRC = 0x04000000,
+
+        /// <summary>
+        /// The file will be added as single unit. Files stored as single unit cannot be encrypted, because Blizzard doesn't support them.
+        /// </summary>
+        MPQ_FILE_SINGLE_UNIT = 0x01000000,
+
+        /// <summary>
+        /// If this flag is specified and the file is already in the MPQ, it will be replaced.
+        /// </summary>
+        MPQ_FILE_REPLACEEXISTING = 0x80000000
+    }
+
+    [Flags]
+    public enum MpqFileAddFileExCompression : uint
+    {
+        NONE = 0x0,
+
+        /// <summary>
+        /// Use Huffman compression. This bit can only be combined with MPQ_COMPRESSION_ADPCM_MONO or MPQ_COMPRESSION_ADPCM_STEREO.
+        /// </summary>
+        MPQ_COMPRESSION_HUFFMANN = 0x01,
+
+        /// <summary>
+        /// Use ZLIB compression library. This bit cannot be combined with MPQ_COMPRESSION_BZIP2 or MPQ_COMPRESSION_LZMA.
+        /// </summary>
+        MPQ_COMPRESSION_ZLIB = 0x02,
+
+        /// <summary>
+        /// Use Pkware Data Compression Library. This bit cannot be combined with MPQ_COMPRESSION_LZMA.
+        /// </summary>
+        MPQ_COMPRESSION_PKWARE = 0x08,
+
+        /// <summary>
+        /// Use BZIP2 compression library. This bit cannot be combined with MPQ_COMPRESSION_ZLIB or MPQ_COMPRESSION_LZMA.
+        /// </summary>
+        MPQ_COMPRESSION_BZIP2 = 0x10,
+
+        /// <summary>
+        /// Use SPARSE compression. This bit cannot be combined with MPQ_COMPRESSION_LZMA.
+        /// </summary>
+        MPQ_COMPRESSION_SPARSE = 0x20,
+
+        /// <summary>
+        /// Use IMA ADPCM compression for 1-channel (mono) WAVE files. This bit can only be combined with MPQ_COMPRESSION_HUFFMANN. 
+        /// This is lossy compression and should only be used for compressing WAVE files.
+        /// </summary>
+        MPQ_COMPRESSION_ADPCM_MONO = 0x40,
+
+        /// <summary>
+        /// Use IMA ADPCM compression for 2-channel (stereo) WAVE files. This bit can only be combined with MPQ_COMPRESSION_HUFFMANN. 
+        /// This is lossy compression and should only be used for compressing WAVE files.
+        /// </summary>
+        MPQ_COMPRESSION_ADPCM_STEREO = 0x80,
+
+        /// <summary>
+        /// Use LZMA compression. This value can not be combined with any other compression method.
+        /// </summary>
+        MPQ_COMPRESSION_LZMA = 0x12,
+
+        /// <summary>
+        /// Same compression - only valid for compressionNext
+        /// </summary>
+        MPQ_COMPRESSION_NEXT_SAME = 0xFF
+    }
+
+
 
     [Flags]
     public enum MpqFileVerificationResults
